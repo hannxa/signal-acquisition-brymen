53610ed8d9700f24f440ebd47d66c51c18189d06
Hanna Baranowska <s193531@student.pg.edu.pl>
2025-05-14 15:24:06 +0200

    skeleton of application

---
+++ signal_acquisition_brymen/RigolFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace signal_acquisition_brymen
+{
+    public class RigolFunction
+    {
+        private SerialPort _port;
+
+        public RigolFunction(SerialPort port)
+        {
+            _port = port;
+        }
+
+        public void GetVoltage()
+        {
+            _port.WriteLine(":MEASure:VOLTage?");
+        }
+
+        public void GetCurrent()
+        {
+            _port.WriteLine(":MEASure:CURRent?");
+        }
+    }
+}
---
+++ signal_acquisition_brymen/SerialPortInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace signal_acquisition_brymen
+{
+    public class SerialPortInitializer
+    {
+        public SerialPort Port { get; private set; }
+
+        public SerialPortInitializer(string portName = "COM1")
+        {
+            Port = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+            Port.DataReceived += Port_DataReceived;
+            Port.Open();
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = Port.ReadExisting();
+            Console.WriteLine($"[DATA RECEIVED]: {data}");
+        }
+    }
+}
